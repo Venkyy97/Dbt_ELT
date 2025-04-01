49af29bea16434ca7886988d0f48a958a6a6e254
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-04-01 14:09:46 -0500

    Added the DAG

---
+++ dbt_dag.py
@@ -0,0 +1,27 @@
+import os
+from datetime import datetime
+
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+
+DBT_PROJECT_DIR = os.path.abspath("/home/venkat/dbt-dag/dbt/dbt_pipeline")
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=SnowflakeUserPasswordProfileMapping(
+        conn_id="snowflake_conn", 
+        profile_args={"database": "dbt_db", "schema": "dbt_schema"},
+    )
+)
+
+dbt_snowflake_dag = DbtDag(
+    project_config=ProjectConfig(DBT_PROJECT_DIR),
+    operator_args={"install_deps": True},
+    profile_config=profile_config,
+    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
+    schedule_interval="@daily",
+    start_date=datetime(2023, 9, 10),
+    catchup=False,
+    dag_id="dbt_dag",
+)
